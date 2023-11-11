0b89b368a604ceb3c487f3899d052b3eaae1b15c
maulanaghozi <maulana.ghozi@qbit.co.id>
2023-11-11 15:35:29 +0700

    move directory screens and assets to src

--- App.tsx
+++ App.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import FeedScreen from './screens/FeedScreen';
-import PostDetailScreen from './screens/PostDetailScreen';
+import FeedScreen from './src/screens/FeedScreen';
+import PostDetailScreen from './src/screens/PostDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
